275a593ac18f3e657698406aad103ea052892ecc
Abu Faris <abufariskolkata@gmail.com>
2025-09-13 12:44:59 +0530

    Added logos to coin selection dropdowns

--- src/components/ComparePage/SelectCoins/index.js
+++ src/components/ComparePage/SelectCoins/index.js
@@ -1,5 +1,5 @@
-import { MenuItem, Select } from "@mui/material";
-import React, { useState } from "react";
+import { MenuItem, Select, Box, Typography } from "@mui/material";
+import React from "react";
 import SelectDays from "../../CoinPage/SelectDays";
 import "./styles.css";
 
@@ -27,6 +27,36 @@ function SelectCoins({
     },
   };
 
+  const renderMenuItem = (coin) => (
+    <MenuItem
+      value={coin.id}
+      key={coin.id}
+      sx={{ display: "flex", alignItems: "center", gap: "0.5rem" }}
+    >
+      <img
+        src={coin.image}
+        alt={coin.name}
+        style={{ width: "20px", height: "20px", borderRadius: "50%" }}
+      />
+      {coin.name}
+    </MenuItem>
+  );
+
+  const renderValue = (selectedId) => {
+    const coin = allCoins.find((c) => c.id === selectedId);
+    if (!coin) return null;
+    return (
+      <Box sx={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
+        <img
+          src={coin.image}
+          alt={coin.name}
+          style={{ width: "20px", height: "20px", borderRadius: "50%" }}
+        />
+        <Typography>{coin.name}</Typography>
+      </Box>
+    );
+  };
+
   return (
     <div className="select-coins-div">
       <div className="select-flex">
@@ -35,32 +65,28 @@ function SelectCoins({
           value={crypto1}
           onChange={(e) => onCoinChange(e, false)}
           sx={style}
+          renderValue={renderValue}
         >
           {allCoins
-            .filter((coin) => coin.id != crypto2)
-            .map((coin, i) => (
-              <MenuItem value={coin.id} key={i}>
-                {coin.name}
-              </MenuItem>
-            ))}
+            .filter((coin) => coin.id !== crypto2)
+            .map(renderMenuItem)}
         </Select>
       </div>
+
       <div className="select-flex">
         <p>Crypto 2</p>
         <Select
           value={crypto2}
           onChange={(e) => onCoinChange(e, true)}
           sx={style}
+          renderValue={renderValue}
         >
           {allCoins
-            .filter((coin) => coin.id != crypto1)
-            .map((coin, i) => (
-              <MenuItem value={coin.id} key={i}>
-                {coin.name}
-              </MenuItem>
-            ))}
+            .filter((coin) => coin.id !== crypto1)
+            .map(renderMenuItem)}
         </Select>
       </div>
+
       <SelectDays
         days={days}
         handleDaysChange={handleDaysChange}
